dd6cc53d2a0b99da2353047d7113ffee29ac604e
Aaditya Mankar <infamous@pop-os.localdomain>
2020-12-06 12:24:45 +0530

    Updated folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import AcmInfo from './components/AcmInfo/AcmInfo';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import News from './components/NewsFlash/News';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <AcmInfo />
+      <News />
+      <Contact />
+      <Footer />
+    </React.Fragment>
   );
 }
 
