bcb8d166362c36a497797a51c15e7d52010acb12
Aaditya Mankar <infamous@pop-os.localdomain>
2020-12-11 15:17:17 +0530

    Changes to Contact.js, Updated font misload.

--- src/components/Dashboard/Contact/Contact.js
+++ src/components/Dashboard/Contact/Contact.js
@@ -1,79 +1,75 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
-const Contact=()=>{
-
-  const [data,setData]=useState({
-    fullname:"",
-    email:"",
-    phone:"",
-    msg:"",
+const Contact = () => {
+  const [data, setData] = useState({
+    fullname: '',
+    email: '',
+    phone: '',
+    msg: '',
   });
 
-    const InputEvent=(event)=>{
-        const {name,value}=event.target;
+  const InputEvent = (event) => {
+    const { name, value } = event.target;
 
-        setData((preVal)=>{
-          return {
-                ...preVal,
-                [name]:value,
-          };
-      });
+    setData((preVal) => {
+      return {
+        ...preVal,
+        [name]: value,
+      };
+  });
 };
 
+const formSubmit = (e) => {
+  e.preventDefault();
+  alert(
+    `Hello ${data.fullname}, Thank you for reaching out to us!`
+  )
+}
 
-    const formSubmit = (e) =>{
-          e.preventDefault();
-          alert(
-            `Hello ${data.fullname}, Thank you for reaching out to us!`
-          )
-    }
+// Please use Row, Col, Container directly from react-bootstrap, I have provided a sample below. Otherwise we'd be underutilizing react's optimisation power.
+// Also when copy-pasting pre-made bootstrap components, make sure to change "class" to "className" and "for" to "htmlFor". I've done it for now as I didn't want reference
+// -- errors. But be careful of that.
+// I've also rearranged the code. Use perfect tabs, make it look pretty in general - it is easier to read and modify.
 
-
-  return (
-    <>
-      
-      <div className="container contact_div shadow-lg p-3 mb-5 bg-white rounded w-50 p-3 border border-primary rounded">
-      <div className="my-5 ">
-        <h1 className="text-center text-primary">Contact Us</h1>
-      </div>
-        <div className="row">
-          <div className="col-md-6 col-10 mx-auto">
-          <div class="mb-3">
-
-          <form onSubmit={formSubmit}>
-          <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label text-primary ">Full Name</label>
-  <input type="text" class="form-control border-aqua" id="exampleFormControlInput1" name="fullname" value={data.fullname} onChange={InputEvent} placeholder="Enter your Name" />
-</div>
-
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label text-primary">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" name="email" value={data.email} onChange={InputEvent} placeholder="Enter your Email Id" />
-</div>
-
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label text-primary">Mobile Number</label>
-  <input type="number" class="form-control" id="exampleFormControlInput1" name="phone" value={data.phone} onChange={InputEvent} placeholder="Enter Mobile Number" />
-</div>
-
-
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label text-primary">Message</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"  name="msg" value={data.msg} onChange={InputEvent}></textarea>
-</div>
-
-<div class="col-12 text-center">
-    <button class="btn btn-outline-primary" type="submit">SEND</button>
-  </div>
-
-          </form>
-  
-          </div>
+return (      
+<div className = 'container-fluid contact_div p-3 mb-5 bg-white rounded p-3'>
+  <Row>
+    <Col sm={12} lg={4} md={4} xl={4}>
+      {/* Map will go here */}
+    </Col>
+    <Col sm={12} lg={8} md={8} xl={8}>
+      <div className = 'my-5 '><h1 className = 'text-center text-primary'>Contact Us</h1></div>
+        <div className = 'row'>
+        <div className = 'col-md-6 col-10 mx-auto'>
+          <div className = 'mb-3'>
+            <form onSubmit = {formSubmit}>
+              <div className = 'mb-3'>
+                <label htmlFor = 'exampleFormControlInput1' className = 'form-label text-primary '>Full Name</label>
+                <input type = 'text' className = 'form-control border-aqua' id = 'exampleFormControlInput1' name = 'fullname' value = {data.fullname} onChange = {InputEvent} placeholder = 'Enter your Name' />
+              </div>
+                <div className = 'mb-3'>
+                <label htmlFor = 'exampleFormControlInput1' className = 'form-label text-primary'>Email address</label>
+                <input type = 'email' className = 'form-control' id = 'exampleFormControlInput1' name = 'email' value = {data.email} onChange = {InputEvent} placeholder = 'Enter your Email Id' />
+              </div>
+              <div className = 'mb-3'>
+                <label htmlFor = 'exampleFormControlInput1' className = 'form-label text-primary'>Mobile Number</label>
+                <input type = 'number' className = 'form-control' id = 'exampleFormControlInput1' name = 'phone' value = {data.phone} onChange = {InputEvent} placeholder = 'Enter Mobile Number' />
+              </div>
+              <div className = 'mb-3'>
+                <label htmlFor = 'exampleFormControlTextarea1' className = 'form-label text-primary'>Message</label>
+                <textarea className = 'form-control' id = 'exampleFormControlTextarea1' rows = '3'  name = 'msg' value = {data.msg} onChange = {InputEvent}></textarea>
+              </div>
+              <div className = 'col-12 text-center'>
+                <button className = 'btn btn-outline-primary' type = 'submit'>SEND</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </>
-  )
-}
+    </Col>
+  </Row>
+</div>
+)}
 
 export default Contact;
