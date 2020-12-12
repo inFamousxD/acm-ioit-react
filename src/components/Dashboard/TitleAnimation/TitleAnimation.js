416b6e6bc7c4d714c7d9ec2aaa09b10d4fae5a62
Aaditya Mankar <infamous@pop-os.localdomain>
2020-12-12 14:48:31 +0530

    animation color updates

--- src/components/Dashboard/TitleAnimation/TitleAnimation.js
+++ src/components/Dashboard/TitleAnimation/TitleAnimation.js
@@ -11,7 +11,7 @@ const TitleAnimation = () => {
             loop: true,
             direction: 'alternate'
         });
-        anim1.add({
+        anim1.add({ //squares swirl in
             targets: '.square',
             translateX: anime.stagger(40, {grid: [32, 5], from: 'center', axis: 'x'}),
             translateY: anime.stagger(40, {grid: [32, 5], from: 'center', axis: 'y'}),
@@ -26,9 +26,9 @@ const TitleAnimation = () => {
             targets: '.square',
             rotateZ: anime.stagger([315, 315], {grid: [32, 5], from: 'center', axis: 'x'}),
             easing: 'easeInOutCirc',
-            delay: anime.stagger(30, {grid: [32, 5], from: 'center'}),
+            delay: anime.stagger(90, {grid: [32, 5], from: 'center'}),
             opacity: [0.75, 0.8],
-            backgroundColor: '#0069cc',
+            backgroundColor: '#405DFF',
             scale: [2, 2],
             borderRadius: anime.stagger(['50%', '50%'], {grid: [32, 5], from: 'center'}),
         }).add({ // back to sq, shrink, change color
@@ -36,8 +36,8 @@ const TitleAnimation = () => {
             rotateZ: anime.stagger([45, 45], {grid: [32, 5], from: 'center', axis: 'x'}),
             easing: 'easeInOutCirc',
             delay: anime.stagger(50, {grid: [32, 5], from: 'center'}),
-            opacity: [0.8, 0.5],
-            backgroundColor: '#006900',
+            opacity: [0.8, 0.65],
+            backgroundColor: '#0081cc',
             scale: [2, 0.8],
             borderRadius: anime.stagger(['0%', '0%'], {grid: [32, 5], from: 'center'}),
         }).add({ // transform into circles
@@ -45,26 +45,26 @@ const TitleAnimation = () => {
             rotateZ: anime.stagger([315, 315], {grid: [32, 5], from: 'center', axis: 'x'}),
             easing: 'easeInOutCirc',
             delay: anime.stagger(30, {grid: [32, 5], from: 'center'}),
-            opacity: [0.5, 0.8],
-            backgroundColor: '#226922',
+            opacity: [0.65, 0.8],
+            backgroundColor: '#005494',
             scale: [0.8, 1.2],
             borderRadius: anime.stagger(['50%', '50%'], {grid: [32, 5], from: 'center'}),
-        }).add({
-            targets: '.square',
-            translateX: anime.stagger(50, {grid: [32, 5], from: 'center', axis: 'x'}),
+        }).add({ //transform to squares
+            targets: '.square', 
+            translateX: anime.stagger(50, {grid: [32, 5], from: 'center', axis: 'y'}),
             translateY: anime.stagger(50, {grid: [32, 5], from: 'center', axis: 'y'}),
             rotateZ: anime.stagger([45, 45], {grid: [32, 5], from: 'center', axis: 'x'}),
             // rotateZ: 225,
             easing: 'easeInOutCirc',
-            delay: anime.stagger(30, {grid: [32, 5], from: 'center'}),
+            delay: anime.stagger(60, {grid: [32, 5], from: 'center'}),
             opacity: [0.8, 0.6],
-            backgroundColor: '#bb2222',
+            backgroundColor: '#009AFF',
             scale: [1.2, 1.6],
             borderRadius: anime.stagger(['0%', '0%'], {grid: [32, 5], from: 'center'}),
         });
     }, []);
     return (
-      <div style={{margin: '10rem 0rem'}}>
+      <div className='square-background' style={{margin: '10rem 0rem'}}>
         <div className='square-grid'>
             <div className="square"></div>
             <div className="square"></div>
