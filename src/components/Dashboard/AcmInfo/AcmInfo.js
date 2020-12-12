a09726685cf5ecc222de3ebe09ab4f795ba72f32
Aaditya Mankar <infamous@pop-os.localdomain>
2020-12-12 17:46:31 +0530

    acm-info update

--- src/components/Dashboard/AcmInfo/AcmInfo.js
+++ src/components/Dashboard/AcmInfo/AcmInfo.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import "./AcmInfo.css"
+import React from 'react';
+import './AcmInfo.css';
+
 const AcmInfo = () => {
-    return (
-        
-        <div className="contain" id = "text">
-            
-           <h1> AISSMS IOIT ACM STUDENT CHAPTER</h1>
+    return (      
+        <div className='content' id = 'text'>
+           <h1 className='acminfo-heading'> AISSMS IOIT ACM STUDENT CHAPTER</h1>
             <p>
                 ACM strengthens the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence.
-             ACM supports the professional growth of its members by providing opportunities for life‐long learning, career development, and professional networking  
+                ACM supports the professional growth of its members by providing opportunities for life‐long learning, career development, and professional networking  
             </p>
-            <p>  ACM IOIT Student Chapter was founded on 2019, with a vision to carry forward ACM’s international intentions on a grass root level at AISSMS’ Institute of Information Technology. 
-             The motive behind starting this Student Chapter was to instill in everyone an intrigue and passion for learning and innovation in technology. Our Chapter, though only recently established and is one the newer Chapters, aims to expand in quality and quantity, and to be instrumental to every student’s technical growth.
-              Our Chapter focuses on enhancing and boosting every student’s technical fervor by frequently organizing various events throughout the year like Technical Competitions, Eminent Speaker Programs, and Discussions with the chapter members. 
-              We are currently a family of 100 members and strive to be an efficient platform for students at an undergraduate level for learning new skills and putting those skills to good use.
+            <p>  
+                ACM IOIT Student Chapter was founded on 2019, with a vision to carry forward ACM’s international intentions on a grass root level at AISSMS’ Institute of Information Technology. 
+                The motive behind starting this Student Chapter was to instill in everyone an intrigue and passion for learning and innovation in technology. Our Chapter, though only recently established and is one the newer Chapters, aims to expand in quality and quantity, and to be instrumental to every student’s technical growth.
+                Our Chapter focuses on enhancing and boosting every student’s technical fervor by frequently organizing various events throughout the year like Technical Competitions, Eminent Speaker Programs, and Discussions with the chapter members. 
+                We are currently a family of 100 members and strive to be an efficient platform for students at an undergraduate level for learning new skills and putting those skills to good use.
             </p>
         </div>
     )
 }
 
-export default AcmInfo
+export default AcmInfo;
