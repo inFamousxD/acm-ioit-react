e991b1fa63cc7093e6fe27d9181e1c69a5800850
Isha Nadkar <ishanadkar@gmail.com>
2020-12-13 12:46:16 +0530

    First Commit

--- src/components/Dashboard/NewsFlash/News.js
+++ src/components/Dashboard/NewsFlash/News.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Container, Card, Row, Col} from 'react-bootstrap';
+import './News.css';
 
 const News = () => {
     return (
-        <div>
-            News
-        </div>
+       <Container>
+           <Row>
+               <Col lg={6}>
+               <Card classname ="md-3" >
+               <Card.Img variant="top" src="https://source.unsplash.com/user/erondu/600x400"/>
+                    <div className = "card-body">
+                        <Card.Title>News</Card.Title>
+                        <Card.Text>
+                         <p> News Description</p>
+                        </Card.Text>
+                        <Card.Link href="#">Read More</Card.Link>
+                    </div>
+                </Card>
+                </Col>
+           </Row>
+       </Container>
     )
 }
-
 export default News
