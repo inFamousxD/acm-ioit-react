1aa5a7036fd34c050eba6e63395ac778b6b3c5c9
inFamousxD <audimankar@gmail.com>
2020-12-19 20:47:08 +0530

    Added sphere looping animation

--- src/components/Dashboard/TitleAnimation/TitleAnimation.js
+++ src/components/Dashboard/TitleAnimation/TitleAnimation.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './TitleAnimation.css';
+import SVGSphere from './SVGSphere';
+import {anime} from 'react-anime';
 
 const TitleAnimation = () => {
+    React.useEffect(() => {
+        let unleashedAnimation = anime.timeline({
+            loop: true,
+            autoplay: true,
+            direction: 'alternate',
+            duration: 5000
+        });
+        unleashedAnimation.add({
+            targets: '.unleashed',
+            color: '#FF6C00',
+            easing: 'easeInOutCirc',
+            delay: anime.stagger(300)
+        }).add({
+            targets: '.unleashed',
+            color: '#09FF00',
+            easing: 'easeInOutCirc',
+            delay: anime.stagger(300)
+        }).add({
+            targets: '.unleashed',
+            color: '#7100FF',
+            easing: 'easeInOutCirc',
+            delay: anime.stagger(300)
+        })
+    }, [])
     return (
-        <div style={{marginTop: '5rem'}}>
-            Title
+        <div style={{paddingTop: '5rem'}} className='title-animation base'>
+            <div className='title-animation-title'>
+                AISSMS IOIT ACM Student Chapter <br /> <div className='unleashed'>#UNLEASHED</div>
+            </div>
+            <SVGSphere />
         </div>
     )
 }
 
-export default TitleAnimation
+export default TitleAnimation;
