2aa5bc9d9dcb076fc63bfa6d804465e8201a68cb
inFamousxD <audimankar@gmail.com>
2020-12-20 13:01:46 +0530

    Added Particle Effect

--- src/components/Dashboard/TitleAnimation/TitleAnimation.js
+++ src/components/Dashboard/TitleAnimation/TitleAnimation.js
@@ -3,6 +3,93 @@ import './TitleAnimation.css';
 import SVGSphere from './SVGSphere';
 import {anime} from 'react-anime';
 import { Row, Col } from 'react-bootstrap';
+import Particles from 'react-particles-js';
+
+const paramsToParticle = {
+    particles: {
+        number: {
+            value: 60,
+            density: {
+            enable: true,
+            value_area: 800
+            }
+        },
+        color: {
+            value: "#00aaff"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000"
+            }
+        },
+        opacity: {
+            value: 0.4,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 2,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable_auto: true,
+            distance: 100,
+            color: "#00aaff",
+            opacity: 1,
+            width: 1,
+            condensed_mode: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 600
+            },
+            shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5
+            }
+        },
+        move: {
+            enable: true,
+            speed: 1,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: true,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detect_on: "window",
+        events: {
+            onhover: {
+                enable: true
+            },
+            onclick: {
+                enable: true
+            },
+            resize: true
+        }
+    },
+    retina_detect: true
+}
 
 const TitleAnimation = () => {
     React.useEffect(() => {
@@ -31,6 +118,9 @@ const TitleAnimation = () => {
     }, [])
     return (
         <Row style={{paddingTop: '5rem'}} className='title-animation base'>
+            <Particles style={{ position: 'absolute', width: '100%', height: '90vh', top: 0, left: 0 }}
+                params={paramsToParticle}
+            ></Particles>
             <Col xs={12} lg={5} md={5} xl={5}>
                 <div className='title-animation-title'>
                     AISSMS IOIT ACM Student Chapter <br /><div className='unleashed'>#UNLEASHED</div>
