4db3bb20a82e34bcf24b5b31a3b0699209f217de
Isha Nadkar <ishanadkar@gmail.com>
2020-12-28 18:26:43 +0530

    Info added to AboutIOIT

--- src/components/Dashboard/AboutIOIT/AboutIOIT.js
+++ src/components/Dashboard/AboutIOIT/AboutIOIT.js
@@ -10,15 +10,11 @@ const AboutIOIT = () => {
         <div style={{ backgroundColor: '#000' }}>
             <Container fluid={true} className='about-ioit-container'>
                 <Row className='about-ioit-header'>
-                    About / Institute of Information Technology
+                    About Institute of Information Technology
                 </Row>
                 <Row className='about-ioit-body'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                    culpa qui officia deserunt mollit anim id est laborum.
+                <p>All India Shri Shivaji Memorial Society’s Institute of Information Technology, Pune was established in August 1999. The college is a self-financing college and is affiliated to Savitribai Phule Pune University (SPPU). It is approved by AICTE and Director of Technical Education, Maharashtra State. The College is centrally located in the heart of Pune City about 1 km from Pune and Shivaji Nagar Railway Stations. The institute enhances the teaching-learning experience by providing students greater resources, contact with industries and academia, employment opportunities and internships.</p>
+                <p>The institute has well qualified and dedicated staff with good infrastructure facilities for UG and PG courses in the form of well-equipped laboratories in all the branches, namely, Computer Engineering, Electrical Engineering, Instrumentation Engineering, Electronics and Telecommunication Engineering, Information Technology and Artificial Intelligence and Data Sciences. The Institute also has Research centre for Electronics and Telecommunication Engineering.</p>
                 </Row>
             </Container>
         </div>
