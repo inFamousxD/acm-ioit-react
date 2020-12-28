61d303e4b0e600332e2b5483c50bfe872d0b3176
inFamousxD <audimankar@gmail.com>
2020-12-28 18:26:26 +0530

    Update

--- src/components/Dashboard/TitleAnimation/TitleAnimation.js
+++ src/components/Dashboard/TitleAnimation/TitleAnimation.js
@@ -122,36 +122,36 @@ const TitleAnimation = () => {
     }, [])
     return (
         <div>
-        <Row style={{paddingTop: '5rem'}} className='title-animation base'>
-            <Particles style={{ position: 'absolute', width: '100%', height: '90vh', top: 0, left: 0, opacity: '0.8' }}
-                params={paramsToParticle}
-            ></Particles>
-            <Col xs={12} lg={5} md={5} xl={5}>
-                <div className='title-animation-title'>
-                    AISSMS IOIT ACM Student Chapter <br /><div className='unleashed'>#UNLEASHED</div>
-                    <div className='unleashed' style={{ fontSize: '2.5vh' }}>• institute • of • information • technology •</div>
-                </div>
-                <SVGSphere />
-            </Col>
-            <Col xs={12} lg={7} md={7} xl={7}>
-                <div className='title-animation-side' style={{ marginTop: '25vh', fontSize: '3.5vh' }}>
-                    Association for Computing Machinery <br />
-                    Advancing Computing as a Science {'&'} Profession   
-                </div>
-                <div className='title-animation-side' style={{
-                    fontSize: '2.5vh',
-                    color: '#fff',
-                    marginTop: '4vh',
-                    marginRight: '2vh',
-                    opacity: '0.9'
-                }}>
-                    ACM brings together computing educators, researchers, and professionals to inspire dialogue, share resources, and address the field's challenges. As the world’s largest computing society, ACM strengthens the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence.   
-                </div>
-                <div className='fa fa-chevron-down title-animation-side scroll-indicator' onClick={scrollTo}></div>
-            </Col>
-            
-        </Row>
-        <div ref={scrollRef}></div>
+            <Row style={{paddingTop: '5rem'}} className='title-animation base'>
+                <Particles style={{ position: 'absolute', width: '100%', height: '90vh', top: 0, left: 0, opacity: '0.8' }}
+                    params={paramsToParticle}
+                ></Particles>
+                <Col xs={12} lg={5} md={5} xl={5}>
+                    <div className='title-animation-title'>
+                        AISSMS IOIT ACM Student Chapter <br /><div className='unleashed'>#UNLEASHED</div>
+                        <div className='unleashed' style={{ fontSize: '2.5vh' }}>• institute • of • information • technology •</div>
+                    </div>
+                    <SVGSphere />
+                </Col>
+                <Col xs={12} lg={7} md={7} xl={7}>
+                    <div className='title-animation-side' style={{ marginTop: '25vh', fontSize: '3.5vh' }}>
+                        Association for Computing Machinery <br />
+                        Advancing Computing as a Science {'&'} Profession   
+                    </div>
+                    <div className='title-animation-side' style={{
+                        fontSize: '2.5vh',
+                        color: '#fff',
+                        marginTop: '4vh',
+                        marginRight: '2vh',
+                        opacity: '0.9'
+                    }}>
+                        ACM brings together computing educators, researchers, and professionals to inspire dialogue, share resources, and address the field's challenges. As the world’s largest computing society, ACM strengthens the profession's collective voice through strong leadership, promotion of the highest standards, and recognition of technical excellence.   
+                    </div>
+                    <div className='fa fa-chevron-down title-animation-side scroll-indicator' onClick={scrollTo}></div>
+                </Col>
+                
+            </Row>
+            <div ref={scrollRef}></div>
         </div>
     )
 }
