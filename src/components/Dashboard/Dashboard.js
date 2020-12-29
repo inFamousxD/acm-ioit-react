ef498db01bc00c8851cb4dd1e268d84164c1c8ce
inFamousxD <audimankar@gmail.com>
2020-12-29 15:37:22 +0530

    Updates

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,12 +4,14 @@ import TitleAnimation from './TitleAnimation/TitleAnimation';
 import Contact from './Contact/Contact';
 import AboutIOIT from './AboutIOIT/AboutIOIT';
 import DigitalLibrary from './DigitalLibrary/DigitalLibrary';
+import News from './NewsFlash/News';
 
 const Dashboard = () => {
     return (
         <div>
             <TitleAnimation />
             <DigitalLibrary />
+            <News />
             <AboutIOIT />
             <Contact />
         </div>
