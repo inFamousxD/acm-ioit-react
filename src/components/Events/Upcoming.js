689d8c72228d8f0ba545c1ae97c2161edf310be4
Isha Nadkar <ishanadkar@gmail.com>
2021-01-21 12:54:57 +0530

    Image Updated

--- src/components/Events/Upcoming.js
+++ src/components/Events/Upcoming.js
@@ -2,7 +2,7 @@ const upcoming = [
     {
         id: "1",
         title: "Technology Trends – A Perspective Synopsis",
-        link: "https://i.ibb.co/3dTcLw7/Techno-Trends.jpg",
+        link: "https://i.ibb.co/CQZ3VdH/Techno-Trends-2.jpg",
         descLong: "This talk provides a broad perspective about the recent technology advances and its impact on Software development.Some coverage on emerging technology areas and its relevance to students will be addressed.",
         descShort: "A webinar by Mr. R Venkateshwar on the Prespective Synopsis of Technology Trends",
         date: "25th January 2021",
