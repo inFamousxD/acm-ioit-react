a7d7c3c5c641f723efcd65fa1e27157dd3ee1656
inF4LLoc <53909861+inFamousxD@users.noreply.github.com>
2021-01-26 22:26:20 +0530

    Update UpcomingEvent.js

--- src/components/Events/components/UpcomingEvent.js
+++ src/components/Events/components/UpcomingEvent.js
@@ -11,7 +11,7 @@ function UpcomingEvent(props) {
             <div className="event-body2">
                 <div className="event">
                     <p className='title'>{props.title}</p>
-                    <p className='title' style={{ color: '#09bb00' }}>Eminent Speaker Program <br />Mr. R Venkateshwar</p>
+                    <p className='title' style={{ color: '#09bb00' }}><br />by Mario Rozario</p>
                     <p className='title'>{props.date}</p>
                     <p className='title'>{props.timing}</p>
                     <p className="event-info">{props.descLong}</p>
