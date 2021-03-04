12f5c14b0e94e1c6083ccd280593c6db685dc2be
Aditya <adityamore360123@gmail.com>
2021-03-04 18:12:06 +0530

    Update DigitalLibrary.js

--- src/components/Dashboard/DigitalLibrary/DigitalLibrary.js
+++ src/components/Dashboard/DigitalLibrary/DigitalLibrary.js
@@ -7,11 +7,11 @@ const DigitalLibrary = () => {
         <React.Fragment>
             <div className="digital-library-head">ACM DIGITAL LIBRARY</div>
             <section class="icons">
-                <a href="http://localhost:3000/acm-ioit-react/Home#" class="iconbtn fas fa-laptop">{" "}</a>
+                <a href="https://learning.acm.org/e-learning/skillsoft" class="iconbtn fas fa-laptop">{" "}</a>
                 <a href="https://dl.acm.org/" class="iconbtn fas fa-book">{" "}</a>
-                <a href="http://localhost:3000/acm-ioit-react/Home#" class="iconbtn fas fa-cog">{" "}</a>
+                <a href="https://idp.acm.org/idp/profile/SAML2/Redirect/SSO?execution=e5s1" class="iconbtn fas fa-cog">{" "}</a>
                 <a href="https://technews.acm.org/" class="iconbtn fas fa-newspaper">{" "}</a>
-                <a href="http://localhost:3000/acm-ioit-react/Home#" class="iconbtn fas fa-flask">{" "}</a>
+                <a href="https://learning.acm.org/e-learning/science-direct" class="iconbtn fas fa-flask">{" "}</a>
                 <a href="https://xrds.acm.org/" class="iconbtn fas fa-link">{" "}</a>
             </section>  
         </React.Fragment>
